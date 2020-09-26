83e9f5936c73af8a6b06b86fc1c1303a87e11a30
akrawczyk <akrawczyk@cad-schroer.de>
2020-09-26 21:14:24 +0200

    File Commit

---
+++ Assets/_Ressources/MainScripts/TerminalManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerminalManager : MonoBehaviour
+{
+    [SerializeField] private GameObject knoten;
+    [SerializeField] private GameObject terminal;
+    [SerializeField] private GameObject zeile;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // erstelle die erste Zeile
+        Instantiate(zeile, this.transform);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(this.tag != "Fokussed") return;
+        else{
+            if(Input.GetKey(KeyCode.LeftAlt) && Input.GetKeyDown(KeyCode.Return))
+            {
+                Debug.Log("Erzeugen");
+                Erzeugen();
+            }
+            if(Input.GetKey(KeyCode.LeftShift) && Input.GetKey(KeyCode.LeftAlt) && Input.GetKeyDown(KeyCode.Q))
+            {
+                Debug.Log("Delete");
+                Delete();
+            }
+            //Restliche Eingabe muss abgefragt werden, damit Befehle funktionieren
+        }
+    }
+
+    public void Erzeugen()
+    {
+        if(this.transform.parent.tag == "Knoten")
+        {
+            GameObject newTerminal =  Instantiate(terminal, this.transform.parent);
+            this.transform.tag = "Untagged";
+            newTerminal.tag = "Fokussed";
+        }
+    }
+
+    public void Delete()
+    {
+        Destroy(this.gameObject);
+    }
+}
