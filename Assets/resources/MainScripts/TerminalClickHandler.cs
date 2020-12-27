58bf05f9ebfe75ec43a9fcefa76668c3c44b0e97
adriankraw <adriankraw1@googlemail.com>
2020-12-27 15:21:40 +0100

    added onClick for Terminal

---
+++ Assets/resources/MainScripts/TerminalClickHandler.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class TerminalClickHandler : MonoBehaviour, IPointerClickHandler
+{
+    [SerializeField] private Transform content;
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        //Debug.Log("I was clicked");
+        content.GetChild(content.childCount-1).gameObject.GetComponentInChildren<InputField>().Select();
+    }
+}
